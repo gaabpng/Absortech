a3c6a1070adafc25595128e934fcc4aff8d7ca70
gaabpng <gabriel.s.rodrigues.ifrj@gmail.com>
2024-07-28 17:45:54 -0300

    feat: mudança no código dos esps

--- Client/src/client.cpp
+++ Client/src/client.cpp
@@ -1,18 +1,47 @@
-#include <Arduino.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const char *ssid = "ESP32_AP";
+const char *password = "12345678";
+const char *serverName = "http://192.168.4.1/";
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+const int pin = 2;
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(pin, INPUT);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  delay(2000);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    HTTPClient http;
+    http.begin(serverName + String(digitalRead(pin)));
+    int httpResponseCode = http.GET();
+    if (httpResponseCode > 0)
+    {
+      String response = http.getString();
+      Serial.println(httpResponseCode);
+      Serial.println(response);
+    }
+    else
+    {
+      Serial.print("Error on sending GET: ");
+      Serial.println(httpResponseCode);
+    }
+    http.end();
+  }
+  delay(2000); // Envia a cada 2 segundos
 }
--- Server/src/server.cpp
+++ Server/src/server.cpp
@@ -1,18 +1,63 @@
-#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiAP.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const char *ssid = "ESP32_AP";
+const char *password = "12345678";
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+WiFiServer server(80);
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Configuring access point...");
+
+  WiFi.softAP(ssid, password);
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+
+  server.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  WiFiClient client = server.available();
+  if (client)
+  {
+    Serial.println("New Client.");
+    String currentLine = "";
+    while (client.connected())
+    {
+      if (client.available())
+      {
+        char c = client.read();
+        Serial.write(c);
+        if (c == '\n')
+        {
+          if (currentLine.length() == 0)
+          {
+            break;
+          }
+          else
+          {
+            if (currentLine.startsWith("GET /"))
+            {
+              int pinValue = currentLine.substring(5, 6).toInt();
+              Serial.print("Pin value received: ");
+              Serial.println(pinValue);
+            }
+            currentLine = "";
+          }
+        }
+        else if (c != '\r')
+        {
+          currentLine += c;
+        }
+      }
+    }
+    client.stop();
+    Serial.println("Client Disconnected.");
+  }
 }
