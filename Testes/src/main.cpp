4c2bd9b80092a547faa2714b1381cb826a1f30ee
gaabpng <gabriel.s.rodrigues.ifrj@gmail.com>
2024-08-12 14:43:39 -0300

    fix: obter MAC

--- Testes/src/main.cpp
+++ Testes/src/main.cpp
@@ -1,20 +1,14 @@
-#include <WiFi.h>
+#include <TFT_eSPI.h>
+#include "WiFi.h"
 
-void setup() {
-  // Inicializa a comunicação serial
-  Serial.begin(115200);
-  
-  // Aguarda a inicialização do monitor serial
-  while (!Serial) {
-    delay(100);
-  }
-  
-  // Obtém e exibe o endereço MAC do ESP32
-  String macAddress = WiFi.macAddress();
-  Serial.println("Endereço MAC do ESP32:");
-  Serial.println(macAddress);
+void setup(){
+
+  //print mac adress
+  WiFi.mode(WIFI_MODE_STA);
+  tft.drawString("my MAC is:",10,10,4);
+  tft.drawString(String(WiFi.macAddress()),10,50,4);
 }
+ 
+void loop(){
 
-void loop() {
-  // Não há necessidade de código no loop para esta tarefa
 }
