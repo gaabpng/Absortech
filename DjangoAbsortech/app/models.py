3239079a622afce1197b9374ce9f7e7ca20331e1
Gabriel Sales Rodrigues <88689778+gaabpng@users.noreply.github.com>
2024-11-16 13:51:44 -0300

    feat: add percentage calculation for distance sensor readings

--- DjangoAbsortech/app/models.py
+++ DjangoAbsortech/app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class LeituraSensor(models.Model):
+    andar = models.CharField(max_length=50)
+    data = models.DateField(auto_now_add=True)
+    hora = models.TimeField(auto_now_add=True)
+    valor_leitura = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.andar} - {self.data} {self.hora} - {self.valor_leitura} cm"
+
+    def calcular_porcentagem(self):
+        """
+        Converte o valor da leitura de centímetros para porcentagem.
+        2 cm = 100%, 30 cm = 0%
+        """
+        MIN_CM = 2
+        MAX_CM = 30
+
+        # Garantir que o valor esteja dentro do intervalo esperado
+        if self.valor_leitura < MIN_CM:
+            return 100.0  # 100% se for menor que o mínimo
+        elif self.valor_leitura > MAX_CM:
+            return 0.0  # 0% se for maior que o máximo
+        
+        # Cálculo da porcentagem
+        porcentagem = ((MAX_CM - float(self.valor_leitura)) / (MAX_CM - MIN_CM)) * 100
+        return round(porcentagem, 2)  # Retorna com 2 casas decimais
