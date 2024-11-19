bca42ef5ca2cad6ba90a21148b280f149ed8d10e
GuilberValerio <102636307+GuilberValerio@users.noreply.github.com>
2024-11-19 17:02:21 -0300

    fix: configuração banco de dados
    Ajustamos a configuração do banco de dados para funcionar com o banco criado no servidor da AWS

--- DjangoAbsortech/DjangoAbsortech/settings.py
+++ DjangoAbsortech/DjangoAbsortech/settings.py
@@ -76,8 +76,16 @@ WSGI_APPLICATION = 'DjangoAbsortech.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'AbsortechDB',
+        'USER': 'usuario',
+        'PASSWORD': 'IEEE-access',
+        'HOST': '172.31.3.2',
+        'PORT': '3306',
+        'OPTIONS': {
+        'charset': 'utf8mb4',
+        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
