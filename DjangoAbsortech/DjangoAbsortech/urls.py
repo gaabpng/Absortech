0f3f163815882c30c9aa7238d394aecc94d73c82
gaabpng <gabriel.s.rodrigues.ifrj@gmail.com>
2025-02-15 11:49:09 -0300

    feat: adicionar exibição das leituras dos sensores

--- DjangoAbsortech/DjangoAbsortech/urls.py
+++ DjangoAbsortech/DjangoAbsortech/urls.py
@@ -17,8 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app import views
+from app.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home)
+    path('',home, views.home)
 ]
--- DjangoAbsortech/app/views.py
+++ DjangoAbsortech/app/views.py
@@ -1,6 +1,12 @@
+from django.db.models import Max
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Leiturasensor
 
 def home(request):
-    return render(request, 'app/index.html')
+    # Obtém a leitura mais recente de cada andar
+    subquery = Leiturasensor.objects.values('andar').annotate(last_entry=Max('data'))
+    leituras_recentes = Leiturasensor.objects.filter(
+        data__in=[item['last_entry'] for item in subquery]
+    ).order_by('andar', '-hora')
+
+    return render(request, 'seu_template.html', {'leituras': leituras_recentes})
