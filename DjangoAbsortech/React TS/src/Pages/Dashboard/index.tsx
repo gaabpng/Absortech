6340715b61eaa228991bdfc87eb5354707a7e704
Emanuel Fontanha <emanuellira@id.uff.br>
2025-04-25 19:11:38 -0300

    Finalizando página de dashboard

--- DjangoAbsortech/React TS/src/Pages/Dashboard/index.tsx
+++ DjangoAbsortech/React TS/src/Pages/Dashboard/index.tsx
@@ -13,14 +13,13 @@ export default function Dashboard () {
                 <button
                     className={styles.buttonHomepage}
                     onClick={() => (navigate("/"))}>
-                    Homepage
+                    ABSORTECH
                 </button>
                 <h1 className={styles.title1}>CS & EPS</h1>
-                <h1 className={styles.title2}>ABSORTECH</h1>
             </header>
 
             <main className={styles.main}>
-                <h1 className={styles.title3}>Contêineres - Prédio de Engenharia</h1>
+                <h1 className={styles.title2}>Contêineres - Prédio de Engenharia</h1>
                 <div className={styles.statusDiv}>
                     <Status />
                 </div>
@@ -33,8 +32,8 @@ export default function Dashboard () {
                     loading="lazy"></iframe>
                 <div className={styles.infoDiv}>
                     <div className={styles.textDiv}>
-                        <p className={styles.text}>Texto descrevendo</p>
-                        <p className={styles.text}>Universidade Federal Fluminense - Instituto de Computação</p>
+                        <p className={styles.text}>IC - Instituto de Computação</p>
+                        <p className={styles.text}>UFF - Universidade Federal Fluminense</p>
                         <p className={styles.text}>Avenida Gal. Milton Tavares de Souza, s/n</p>
                         <p className={styles.text}>São Domingos, Niterói, RJ - 24210-310</p>
                     </div>
@@ -42,14 +41,22 @@ export default function Dashboard () {
                         <a 
                             href="https://www.instagram.com/cseps.uff/" 
                             target="_blank"
-                            className={styles.linkCSEPS}>
-                            <img src={instagram} alt="Link para a CS & EPS" title="Logo do Instagram"/>
+                            className={styles.link}>
+                            <img 
+                                src={instagram} 
+                                alt="Link para a CS & EPS"
+                                className={styles.image}
+                                title="Logo do Instagram"/>
                             CS & EPS</a>
                         <a 
                             href="https://www.instagram.com/ieee.uff/" 
                             target="_blank"
-                            className={styles.linkRamoUFF}>
-                            <img src={instagram} alt="Link para o Ramo IEEE UFF" title="Logo do Instagram"/>
+                            className={styles.link}>
+                            <img 
+                                src={instagram} 
+                                alt="Link para o Ramo IEEE UFF"
+                                className={styles.image}
+                                title="Logo do Instagram"/>
                             Ramo IEEE UFF</a>
                     </div>
                 </div>
--- DjangoAbsortech/React TS/src/componentes/Status/index.tsx
+++ DjangoAbsortech/React TS/src/componentes/Status/index.tsx
@@ -1,11 +1,53 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import styles from "./styles.module.css"
+
+interface Leitura {
+    andar: string;
+    data: string;
+    hora: string;
+    valor_leitura: number;
+}
 
 export default function Status () {
     
-    
-    
+    const [leituras, setLeituras] = useState<Leitura[]>([]);
+
+    useEffect(() => {
+        const fetchData = () => {
+            axios
+                .get<Leitura[]>("http://localhost:8000/api/leituras/")
+                .then((response) => {
+                    setLeituras(response.data);
+                })
+                .catch((error) => {
+                    console.error("Erro ao buscar leituras: ", error);
+                });
+        };
+
+        fetchData(); // Busca os dados imediatamente ao carregar a página
+        const interval = setInterval(fetchData, 5000); // Atualiza a cada 5 segundos
+
+        return () => clearInterval(interval); // Limpa o intervalo ao desmontar o componente
+    }, []);
+    // <p>Não há dados disponíveis.</p>
     return (
         <div>
-            STATUS: A FAZER
+            {leituras.length > 0 ? (
+                leituras.map((item, index) => (
+                    <div key={index} className={styles.container}>
+                        <p className={styles.text1}>{item.andar}</p>
+                        <div className={styles.statusDiv}>
+                            <p className={styles.text2}>
+                                {item.valor_leitura > 3 ? "• Condição estável." : "• Alerta."}
+                                - {item.valor_leitura} absorvente(s)</p>
+                            <div className={item.valor_leitura >= 3 ? styles.greenSignal : styles.redSignal}></div>
+                        </div>
+                    </div>
+                ))
+            ) : (
+                <p className={styles.text1}>Não há dados disponíveis.</p>
+            )}
         </div>
     )
 }
